0b8f8ca4c962f8abec6fda39d4548b000c158b83
RashidBaghirov <rashidtb@code.edu.az>
2023-02-06 20:45:47 +0400

    ...

--- assets/js/select.js
+++ assets/js/select.js
@@ -108,7 +108,6 @@ cart.addEventListener("click", function (product) {
       let li = this.parentElement;
       let src = li.querySelector(".cart-image img").src;
       basket = basket.filter((device) => device.src != src);
-      li.remove();
       ShowTotalPrice(basket);
       ShowProductCount(basket);
       localStorage.setItem("basket", JSON.stringify(basket));
